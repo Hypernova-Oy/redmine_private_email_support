56eb68056a852427b8b569367eb975d545af04db
Lari Taskula <lari.taskula@hypernova.fi>
2020-02-10 10:56:09 +0000

    Relate issues after saving issue first
    Otherwise unable to get issue.id

--- lib/mail_handler_patch.rb
+++ lib/mail_handler_patch.rb
@@ -32,21 +32,21 @@ module MailHandlerPatch
       issue.is_private = true
     end
 
-    relation = IssueRelation.new
-    relation.issue_from = related_issue
-    relation.issue_to = issue
-    relation.init_journals(User.current)
-
-    begin
-      saved = relation.save
-    rescue ActiveRecord::RecordNotUnique
-      saved = false
-      relation.errors.add :base, :taken
-    end
-
     # add To and Cc as watchers before saving so the watchers can reply to Redmine
     add_watchers(issue)
     issue.save!
+    begin
+      relation = IssueRelation.new
+      relation.issue_from = related_issue
+      relation.issue_to = issue
+      relation.init_journals(User.current)
+      saved = relation.save!
+      logger&.info "MailHandlerPatch: created relation between ##{issue.id} and ##{related_issue.id}"
+    rescue ActiveRecord::RecordNotUnique
+      logger&.warn "MailHandlerPatch: could not create relation ##{issue.id} created by #{user}"
+      saved = false
+      relation.errors.add :base, :taken
+    end
     add_attachments(issue)
     logger&.info "MailHandlerPatch: issue ##{issue.id} created by #{user}"
     issue
